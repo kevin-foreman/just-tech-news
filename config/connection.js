d6f19ffa750156320bb7387c4318d11259a9bf34
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-03 17:19:06 -0500

    conflict resolved

--- config/connection.js
+++ config/connection.js
@@ -1,25 +1,7 @@
-const Sequelize = require('sequelize');
+clearconst Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-<<<<<<< HEAD
-// Create connection to our database, pass in your mysql information for username and password
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-
-} else {
-
-    sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-
-    host: 'localhost',
-
-    dialect: 'mysql',
-    
-    port: 3306
-});
-=======
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
@@ -30,7 +12,6 @@ if (process.env.JAWSDB_URL) {
         dialect: 'mysql',
         port: 3306
     });
->>>>>>> develop
 }
 
 module.exports = sequelize;
