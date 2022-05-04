e50d04a5e3fa9a8ce0b1a1a03eaaa72d7cfd1b28
kevin-foreman <kevin.o.foreman2@gmail.com>
2022-05-03 20:13:14 -0500

    adding to main to push to heroku

--- server.js
+++ server.js
@@ -1,13 +1,10 @@
 const path = require('path');
 const express = require('express');
-const routes = require('./controllers');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
 
 const exphbs = require('express-handlebars');
 const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -15,6 +12,8 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.urlencoded({ extended: true }));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 // turn on routes
 app.use(routes);
